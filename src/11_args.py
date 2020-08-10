63233f3b7df8a7d84a50acf1b5a5ff0c8d4bd32e
Carl Redding <credding07@gmail.com>
2020-08-10 10:21:05 -0500

    back to it. reps reps reps

--- src/11_args.py
+++ src/11_args.py
@@ -15,17 +15,18 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 def f2(*args):
-    return sum(args)
+    for arg in args:
+        return sum(args)
+    # return sum(args)
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
 print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
-# print(a)
+
 a = [7, 6, 5, 4]
-
-
+# print(a)
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))  # Should print 22
+print(f2(sum(a)))  # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -33,10 +34,12 @@ print(f2(a))  # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+# def f3(arg1 = 0, arg2 = 0):
+    
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
 
+# print(f3(1, 2))  # Should print 3
+# print(f3(8))     # Should print 9
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
